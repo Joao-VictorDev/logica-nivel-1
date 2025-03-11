4f47dd17724b85eb6972ef581bb499843052fc3b
João Victor <joaovictor.dev.io@gmail.com>
2025-03-11 10:50:38 -0300

    Create Ex03.py

---
+++ Ex03.py
@@ -0,0 +1,8 @@
+#3. Letra de música - Faça um programa que mostre na tela uma letra de música que você gosta (proibido letras do Justin Bieber).
+print("Artista: MC Cabelinho")
+print("Musica: Carta Aberta")
+print("""letra: Nem um pouco preocupado
+Com a vida alheia (vida alheia)
+Pois só minha vida me interessa
+Só minha vida me interessa
+Oh-oh-oh, oh-oh, oh-oh, oh-oh""")
