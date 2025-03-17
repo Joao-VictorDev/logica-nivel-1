88bbfc288f4d32604fc6140a2fa054eaf5ecf468
João Victor <joaovictor.dev.io@gmail.com>
2025-03-17 19:32:17 -0300

    Create Ex09.py

---
+++ Ex09.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Digite a Primeira Nota"))
+nota2 = float(input("Digite a Segunda Nota"))
+nota3 = float(input("Digite a Terceira Nota"))
+nota4 = float(input("Digite a Quarta Nota"))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print("media do aluno",media)
